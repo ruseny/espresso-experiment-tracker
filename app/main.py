76b19269bbbc7682da208635a9b94553f0b36ede
ruseny <rusen138@gmail.com>
2025-03-27 15:54:28 +0100

    basic app structure and templates created

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+from datetime import datetime
+
+app = FastAPI()
+templates = Jinja2Templates(directory = "../templates")
+
+current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+@app.get("/", response_class = HTMLResponse)
+async def display_home_page(request: Request):
+    return templates.TemplateResponse(
+        request = request, 
+        name = "home.html", 
+        context = {
+            "current_time" : current_time
+        }
+    )
