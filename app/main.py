45b2fb8692a896ef2f2505b6fdaed403724202f2
ruseny <rusen138@gmail.com>
2025-03-27 17:51:52 +0100

    basic form for entering experiment created

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+
+from typing import Annotated
 
 from datetime import datetime
 
@@ -26,5 +28,45 @@ async def display_experiment_page(request : Request, id : int = 0):
         name = "show_experiment.html",
         context = {
             "experiment_id" : id
+        }
+    )
+
+@app.get("/new_experiment", response_class = HTMLResponse)
+async def new_experiment_page(request : Request):
+    return templates.TemplateResponse(
+        request = request, 
+        name = "new_experiment.html"
+    )
+
+@app.post("/new_experiment", response_class = HTMLResponse)
+async def enter_new_experiment(
+    request : Request, 
+    coffee_beans : Annotated[str, Form()],
+    grind_level : Annotated[int, Form()], 
+    amount_gr : Annotated[float, Form()], 
+    extr_time_sec : Annotated[int, Form()], 
+    outp_ml : Annotated[float, Form()]
+):
+    
+    extr_ratio = round(outp_ml / amount_gr, 2)
+
+    if extr_time_sec < 25:
+        extr_time_message = "Too short extraction time"
+    elif extr_time_sec > 30:
+        extr_time_message = "Too long extraction time"
+    else:
+        extr_time_message = "Good extraction time"
+
+    return templates.TemplateResponse(
+        request = request, 
+        name = "new_experiment.html", 
+        context = {
+            "coffee_beans" : coffee_beans,
+            "grind_level" : grind_level,
+            "amount_gr" : amount_gr,
+            "extr_time_sec" : extr_time_sec,
+            "outp_ml" : outp_ml,
+            "extr_ratio" : extr_ratio, 
+            "extr_time_message" : extr_time_message
         }
     )
