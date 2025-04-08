7b35c09fda5bfb5a5c6ea24c144eeebc4cf5b439
ruseny <rusen138@gmail.com>
2025-04-08 13:49:03 +0200

    app connected to the database

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request, Form, Depends
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi import status
 
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, Session, SQLModel, create_engine, text, select
 from enum import Enum
 
 from pydantic import BaseModel
@@ -18,40 +18,21 @@ templates = Jinja2Templates(directory = "../templates")
 
 coffee_beans_purchase_mapper = {1 : "Lavazza", 2 : "Segafredo", 3 : "Tchibo"}
 
-
-
-@app.get("/", response_class = HTMLResponse)
-async def display_home_page(request: Request):
-    return templates.TemplateResponse(
-        request = request, 
-        name = "home.html"
-    )
-
-@app.get("/show_experiment", response_class = HTMLResponse)
-async def display_experiment_page(request : Request):
-    with open("experiment_data.json", "r") as f:
-        experiment_data = json.load(f)
-    return templates.TemplateResponse(
-        request = request, 
-        name = "show_experiment.html",
-        context = experiment_data
-    )
-
-@app.get("/new_experiment", response_class = HTMLResponse)
-async def new_experiment_page(request : Request):
-    return templates.TemplateResponse(
-        request = request, 
-        name = "new_experiment.html",
-        context = {
-            "coffee_beans_purchase_mapper" : coffee_beans_purchase_mapper
-        }
-    )
+with open("credentials/db_login.json", "r") as f:
+    db_login = json.load(f)
+db_path = f"mysql://{db_login['user']}:{db_login['password']}@{db_login['host']}/{db_login['db_name']}"
+db_engine = create_engine(db_path)
+def get_session():
+    with Session(db_engine) as session:
+        yield session
+SessionDep = Annotated[Session, Depends(get_session)]
 
 class YesNo(str, Enum):
     yes = "yes"
     no = "no"
 
 class EspressoExperiments(SQLModel, table = True):
+    __tablename__ = "EspressoExperiments"
     id : Optional[int] = Field(default=None, primary_key=True)
     experiment_datetime : datetime = Field(default_factory = datetime.now)
     setup_id : int = Field(default = 1, foreign_key = "EquipmentSetup.id")
@@ -70,12 +51,69 @@ class EspressoExperiments(SQLModel, table = True):
     evaluation_crema : Optional[int] = Field(default = None, ge = 1, le = 10)
     evaluation_notes : Optional[str] = Field(default = None)
 
+class CoffeeBeanPurchases(SQLModel, table = True):
+    __tablename__ = "CoffeeBeanPurchases"
+    id : Optional[int] = Field(default = None, primary_key = True)
+    user_id : int = Field(default = 1, foreign_key = "Users.id")
+    variety_id : int = Field(default = 1, foreign_key = "CoffeeBeanVarieties.id")
+    purchase_date : date = Field(default_factory = date.today)
+    purchased_from : str = Field(default = None)
+    roast_date : date = Field(default = None)
+    weight_kg : float = Field(default = None)
+    price_per_kg_eur : float = Field(default = None)
+
+class EquipmentSetup(SQLModel, table = True):
+    __tablename__ = "EquipmentSetup"
+    id : Optional[int] = Field(default = None, primary_key = True)
+    user_id : int = Field(default = 1, foreign_key = "Users.id")
+    coffee_machine_id : int = Field(default = 1, foreign_key = "EspressoMachines.id")
+    grinder_id : int = Field(default = 1, foreign_key = "Grinders.id")
+    portafilter_id : int = Field(default = 1, foreign_key = "WaterFilters.id")
+    setup_name : Optional[str] = Field(default = None)
+
+
+
+@app.get("/", response_class = HTMLResponse)
+async def display_home_page(request: Request):
+    return templates.TemplateResponse(
+        request = request, 
+        name = "home.html"
+    )
+
+@app.get("/show_experiment", response_class = HTMLResponse)
+async def display_experiment_page(
+    request : Request, 
+    session : SessionDep
+):
+    query = select(EspressoExperiments).order_by(EspressoExperiments.experiment_datetime.desc()).limit(1)
+    last_entry = session.exec(query).one()
+    return templates.TemplateResponse(
+        request = request, 
+        name = "show_experiment.html",
+        context = last_entry.model_dump(mode = "json")
+    )
+
+@app.get("/new_experiment", response_class = HTMLResponse)
+async def new_experiment_page(request : Request):
+    return templates.TemplateResponse(
+        request = request, 
+        name = "new_experiment.html",
+        context = {
+            "coffee_beans_purchase_mapper" : coffee_beans_purchase_mapper
+        }
+    )
+
+
+
+
 @app.post("/new_experiment", response_class = RedirectResponse)
 async def enter_new_experiment(
     request : Request, 
-    form_data : Annotated[EspressoExperiments, Form()]
+    form_data : Annotated[EspressoExperiments, Form()],
+    session : SessionDep
 ):
-    with open("experiment_data.json", "w") as f:
-        f.write(f"{form_data.model_dump_json()}")
+    session.add(form_data)
+    session.commit()
+    session.refresh(form_data)
 
     return RedirectResponse(url = "/show_experiment", status_code=status.HTTP_302_FOUND)
