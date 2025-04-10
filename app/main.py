7c1f36d86584377ab1978640cddc33decccc698e
ruseny <rusen138@gmail.com>
2025-04-10 12:47:30 +0200

    page showing last experiment updated

--- app/main.py
+++ app/main.py
@@ -45,10 +45,30 @@ async def enter_user(user_id : Annotated[int, Form()]):
 async def display_experiment_page(request : Request, session : SessionDep):
     query = select(EspressoExperiments).order_by(EspressoExperiments.id.desc()).limit(1)
     last_entry = session.exec(query).one()
+    context_dict = last_entry.model_dump(mode = "json")
+
+    user_id = context_dict["user_id"]
+    user_name = user_dict[context_dict["user_id"]]
+
+    machine_dict = get_coffee_machine_dict(user_id)
+    machine_name = machine_dict[context_dict["coffee_machine_id"]]
+    grinder_dict = get_grinder_dict(user_id)
+    grinder_name = grinder_dict[context_dict["grinder_id"]]
+    portafilter_dict = get_portafilter_dict(user_id)
+    portafilter_name = portafilter_dict[context_dict["portafilter_id"]]
+    purchase_dict = get_purchase_dict(user_id)
+    coffee_name = purchase_dict[context_dict["coffee_bean_purchase_id"]]
+
+    context_dict["user_name"] = user_name
+    context_dict["machine_name"] = machine_name
+    context_dict["grinder_name"] = grinder_name
+    context_dict["portafilter_name"] = portafilter_name
+    context_dict["coffee_name"] = coffee_name
+
     return templates.TemplateResponse(
         request = request, 
         name = "show_experiment.html",
-        context = last_entry.model_dump(mode = "json")
+        context = context_dict
     )
 
 @app.get("/new_experiment", response_class = HTMLResponse)
