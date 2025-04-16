ded0056d4b9315435dc92e97e0801b1e1cbd43ee
ruseny <rusen138@gmail.com>
2025-04-16 18:02:48 +0200

    db connection details moved to dotenv file

--- app/dependencies/db_session.py
+++ app/dependencies/db_session.py
@@ -1,11 +1,19 @@
-import json
+from dotenv import load_dotenv
+import os
 from sqlmodel import create_engine, Session
 from typing import Annotated
 from fastapi import Depends
 
-with open("credentials/db_login.json", "r") as f:
-    db_login = json.load(f)
-db_path = f"mysql://{db_login['user']}:{db_login['password']}@{db_login['host']}/{db_login['db_name']}"
+load_dotenv()
+db_login = {
+    "user": os.getenv("DB_USERNAME"),
+    "password": os.getenv("DB_PASSWORD"),
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
+    "db_name": os.getenv("DB_NAME")
+}
+
+db_path = f"mysql://{db_login['user']}:{db_login['password']}@{db_login['host']}:{db_login['port']}/{db_login['db_name']}"
 db_engine = create_engine(db_path)
 def get_session():
     with Session(db_engine) as session:
