4bcfd80189ba6ec13ba5c2097add9180cc83520f
ruseny <rusen138@gmail.com>
2025-04-16 17:43:11 +0200

    checkin, homepage, and links have been streamlined

--- app/main.py
+++ app/main.py
@@ -23,25 +23,44 @@ app.state.current_user = 0
 user_dict = get_user_dict()
 
 
-@app.get("/", response_class = HTMLResponse)
-async def display_home_page(request: Request):
+@app.get("/checkin", response_class = HTMLResponse)
+async def checkin_page(request : Request):
     return templates.TemplateResponse(
         request = request, 
-        name = "home.html", 
+        name = "checkin.html", 
         context = {
             "user_dict" : user_dict
         }
     )
 
-@app.post("/", response_class = RedirectResponse)
+@app.post("/checkin", response_class = RedirectResponse)
 async def enter_user(user_id : Annotated[int, Form()]):
     app.state.current_user = user_id
-    return RedirectResponse(url = "/new_experiment", status_code=status.HTTP_302_FOUND)
+    return RedirectResponse(
+        url = "/", 
+        status_code=status.HTTP_302_FOUND
+    )
+
+@app.get("/", response_class = HTMLResponse)
+async def display_home_page(request: Request):
+    if app.state.current_user == 0:
+        return RedirectResponse(
+            url = "/checkin", 
+            status_code=status.HTTP_302_FOUND
+        )
+    user_id = app.state.current_user
+    user_name = user_dict[user_id]
+    return templates.TemplateResponse(
+        request = request, 
+        name = "home.html", 
+        context = {
+            "user_id" : user_id,
+            "current_user" : user_name
+        }
+    )
 
 @app.get("/new_coffee_machine", response_class = HTMLResponse)
 async def new_coffee_machine_page(request : Request):
-    if app.state.current_user == 0:
-        return RedirectResponse(url = "/", status_code=status.HTTP_302_FOUND)
     return templates.TemplateResponse(
         request = request, 
         name = "new_coffee_machine.html"
@@ -64,8 +83,6 @@ async def enter_new_coffee_machine(
 
 @app.get("/new_grinder", response_class = HTMLResponse)
 async def new_grinder_page(request : Request):
-    if app.state.current_user == 0:
-        return RedirectResponse(url = "/", status_code=status.HTTP_302_FOUND)
     return templates.TemplateResponse(
         request = request, 
         name = "new_grinder.html"
@@ -88,15 +105,13 @@ async def enter_new_grinder(
 
 @app.get("/new_portafilter", response_class = HTMLResponse)
 async def new_portafilter_page(request : Request):
-    if app.state.current_user == 0:
-        return RedirectResponse(url = "/", status_code=status.HTTP_302_FOUND)
     return templates.TemplateResponse(
         request = request, 
         name = "new_portafilter.html"
     )
 
 @app.post("/new_portafilter", response_class = HTMLResponse)
-async def enter_new_grinder(
+async def enter_new_portafilter(
         form_data : Annotated[Portafilters, Form()],
         session : SessionDep, 
         request : Request
@@ -113,12 +128,17 @@ async def enter_new_grinder(
 @app.get("/own_equipment", response_class = HTMLResponse)
 async def owned_equipment_page(request : Request):
     if app.state.current_user == 0:
-        return RedirectResponse(url = "/", status_code=status.HTTP_302_FOUND)
+        return RedirectResponse(
+            url = "/checkin", 
+            status_code=status.HTTP_302_FOUND
+        )
+    
     user_id = app.state.current_user
     user_name = user_dict[user_id]
     machine_dict = get_all_coffee_machines_dict()
     grinder_dict = get_all_grinders_dict()
     portafilter_dict = get_all_portafilters_dict()
+
     return templates.TemplateResponse(
         request = request, 
         name = "own_equipment.html",
@@ -131,11 +151,11 @@ async def owned_equipment_page(request : Request):
         }
     )
 
-@app.post("/own_equipment", response_class = RedirectResponse)
+@app.post("/own_equipment", response_class = HTMLResponse)
 async def enter_owned_equipment(
+    request : Request,
     form_data : Annotated[EquipmentOwnership, Form()], 
-    session : SessionDep,
-    request : Request
+    session : SessionDep
 ):
     if form_data.equipment_type == "coffee machine":
         form_data.grinder_id = None
@@ -150,15 +170,24 @@ async def enter_owned_equipment(
     session.add(form_data)
     session.commit()
     session.refresh(form_data)
-    return RedirectResponse(
-        url = "/own_equipment",
-        status_code = status.HTTP_302_FOUND
+
+    context_dict = form_data.model_dump(mode = "json")
+    context_dict["user_id"] = app.state.current_user
+    context_dict["current_user"] = user_dict[app.state.current_user]
+
+    return templates.TemplateResponse(
+        request = request,
+        name = "own_equipment.html",
+        context = context_dict
     )
 
 @app.get("/user_defaults", response_class = HTMLResponse)
 async def user_defaults_page(request : Request, session : SessionDep):
     if app.state.current_user == 0:
-        return RedirectResponse(url = "/", status_code=status.HTTP_302_FOUND)
+        return RedirectResponse(
+            url = "/checkin", 
+            status_code=status.HTTP_302_FOUND
+        )
     user_id = app.state.current_user
     user_name = user_dict[user_id]
 
@@ -173,11 +202,11 @@ async def user_defaults_page(request : Request, session : SessionDep):
     portafilter_dict = get_portafilter_dict(user_id)
     portafilter_name = portafilter_dict[context_dict["portafilter_id"]]
 
+    context_dict["user_id"] = user_id
     context_dict["current_user"] = user_name
     context_dict["machine_name"] = machine_name
     context_dict["grinder_name"] = grinder_name
     context_dict["portafilter_name"] = portafilter_name
-    context_dict["user_id"] = user_id
     context_dict["machine_dict"] = machine_dict
     context_dict["grinder_dict"] = grinder_dict
     context_dict["portafilter_dict"] = portafilter_dict
@@ -212,8 +241,6 @@ async def enter_user_defaults(
 
 @app.get("/new_coffee_beans", response_class = HTMLResponse)
 async def new_coffee_bean_page(request : Request):
-    if app.state.current_user == 0:
-        return RedirectResponse(url = "/", status_code=status.HTTP_302_FOUND)
     return templates.TemplateResponse(
         request = request, 
         name = "new_coffee_beans.html"
@@ -237,10 +264,13 @@ async def enter_new_coffee_bean(
 @app.get("/new_coffee_purchase", response_class = HTMLResponse)
 async def new_coffee_purchase_page(request : Request):
     if app.state.current_user == 0:
-        return RedirectResponse(url = "/", status_code=status.HTTP_302_FOUND)
+        return RedirectResponse(
+            url = "/checkin", 
+            status_code=status.HTTP_302_FOUND
+        )
+    
     user_id = app.state.current_user
     user_name = user_dict[user_id]
-
     variety_dict = get_all_coffees_dict()
 
     return templates.TemplateResponse(
@@ -284,7 +314,11 @@ async def enter_new_coffee_purchase(
 @app.get("/new_experiment", response_class = HTMLResponse)
 async def new_experiment_page(request : Request, session : SessionDep):
     if app.state.current_user == 0:
-        return RedirectResponse(url = "/", status_code=status.HTTP_302_FOUND)
+        return RedirectResponse(
+            url = "/", 
+            status_code=status.HTTP_302_FOUND
+        )
+    
     user_id = app.state.current_user
     user_name = user_dict[user_id]
 
@@ -323,10 +357,19 @@ async def enter_new_experiment(
     session.add(form_data)
     session.commit()
     session.refresh(form_data)
-    return RedirectResponse(url = "/new_experiment/rate", status_code=status.HTTP_302_FOUND)
+    return RedirectResponse(
+        url = "/new_experiment/rate", 
+        status_code=status.HTTP_302_FOUND
+    )
 
 @app.get("/new_experiment/rate", response_class = HTMLResponse)
 async def experiment_rating_page(request : Request, session : SessionDep):
+    if app.state.current_user == 0:
+        return RedirectResponse(
+            url = "/checkin", 
+            status_code=status.HTTP_302_FOUND
+        )
+    
     query = select(EspressoExperiments).order_by(EspressoExperiments.id.desc()).limit(1)
     last_entry = session.exec(query).one()
     context_dict = last_entry.model_dump(mode = "json")
