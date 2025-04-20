00a6ddfd2966cd711e03c49ad3cccb2131d5efe2
ruseny <rusen138@gmail.com>
2025-04-20 21:45:17 +0200

    mysql connector specified

--- app/dependencies/db_session.py
+++ app/dependencies/db_session.py
@@ -13,7 +13,7 @@ db_login = {
     "db_name": os.getenv("DB_NAME")
 }
 
-db_path = f"mysql://{db_login['user']}:{db_login['password']}@{db_login['host']}:{db_login['port']}/{db_login['db_name']}"
+db_path = f"mysql+pymysql://{db_login['user']}:{db_login['password']}@{db_login['host']}:{db_login['port']}/{db_login['db_name']}"
 db_engine = create_engine(db_path)
 def get_session():
     with Session(db_engine) as session:
