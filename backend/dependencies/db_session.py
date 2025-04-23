be769022a57dc36342589844ee5e9af3ccbdc676
ruseny <rusen138@gmail.com>
2025-04-23 18:36:31 +0200

    env var use modified

--- backend/dependencies/db_session.py
+++ backend/dependencies/db_session.py
@@ -5,15 +5,17 @@ from typing import Annotated
 from fastapi import Depends
 
 load_dotenv()
-db_login = {
-    "user": os.getenv("DB_USERNAME"),
-    "password": os.getenv("DB_PASSWORD"),
-    "host": os.getenv("DB_HOST"),
-    "port": os.getenv("DB_PORT"),
-    "db_name": os.getenv("DB_NAME")
-}
 
-db_path = f"mysql+pymysql://{db_login['user']}:{db_login['password']}@{db_login['host']}:{db_login['port']}/{db_login['db_name']}"
+
+driver = os.getenv("DB_DRIVER")
+connector = os.getenv("DB_CONNECTOR")
+user = os.getenv("DB_USERNAME")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+
+db_path = f"{driver}+{connector}://{user}:{password}@{host}:{port}/{db_name}"
 db_engine = create_engine(db_path, echo=True)
 def get_session():
     with Session(db_engine) as session:
